cc5622c39e78c2235a22f962d2303015e018ba75
LotosPage <34channel@mail.ru>
2019-02-09 19:56:24 +0500

    hello world3

--- tEST/src/main/java/ru/stqa/pft/MyFirstProgram.java
+++ tEST/src/main/java/ru/stqa/pft/MyFirstProgram.java
@@ -3,6 +3,6 @@ package ru.stqa.pft;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World3!");
     }
 }
