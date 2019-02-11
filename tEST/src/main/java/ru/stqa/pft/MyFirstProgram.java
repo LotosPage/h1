f6bacca4cfd59cc73e54d64a75962d9c13e003a7
LotosPage <34channel@mail.ru>
2019-02-11 21:20:56 +0500

    added some function

--- tEST/src/main/java/ru/stqa/pft/MyFirstProgram.java
+++ tEST/src/main/java/ru/stqa/pft/MyFirstProgram.java
@@ -1,12 +1,24 @@
 package ru.stqa.pft;
 
+import java.sql.SQLOutput;
+
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello World3!");
+        double l = 5;
+        System.out.println("Площадь квадрта со стороной " + l + " = " + area(l));
 
-
-        System.out.println("ssdfddddddd");
-
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
     }
+
+    public static double area(double len) {
+        return len * len;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
+
 }
