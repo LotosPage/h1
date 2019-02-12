0d1966a9a92f65e78a09fa4b27fb48f3a0592c7f
LotosPage <34channel@mail.ru>
2019-02-12 20:00:02 +0500

    added some test and library

--- tEST/src/main/java/ru/stqa/pft/MyFirstProgram.java
+++ tEST/src/main/java/ru/stqa/pft/MyFirstProgram.java
@@ -5,20 +5,14 @@ import java.sql.SQLOutput;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        double l = 5;
-        System.out.println("Площадь квадрта со стороной " + l + " = " + area(l));
+        Square s = new Square(5);
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+        System.out.println("Площадь квадрта со стороной " + s.l + " = " + s.area());
+
+        Rectangle r = new Rectangle(4, 6);
+
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
     }
 
-    public static double area(double len) {
-        return len * len;
-    }
-
-    public static double area(double a, double b) {
-        return a * b;
-    }
 
 }
